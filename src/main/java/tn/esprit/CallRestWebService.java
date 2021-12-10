8543506b5fb5067eb616c81b6378614a70583da4
Amina CHABCHOUB <chabchoub.amina@esprit.tn>
2021-12-10 12:58:37 +0100

    copie 1 branche

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -21,6 +21,7 @@ import org.json.JSONObject;
 
 public class CallRestWebService {
 // copie el branche
+	// copie el branche ll git
 	public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
 	public static void main(String[] args) {
